bea2124cd82a8ef9b28a226c4fafa14681e0cdf5
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2025-07-23 16:26:19 +0200

    04e05

--- exercises/04_traits/05_trait_bounds/src/lib.rs
+++ exercises/04_traits/05_trait_bounds/src/lib.rs
@@ -4,9 +4,12 @@
 // Note: there are different trait bounds that'll make the compiler happy, but they come with
 // different _semantics_. We'll cover those differences later in the course when we talk about ordered
 // collections (e.g. BTreeMap).
+//
+
+use std::cmp::PartialOrd;
 
 /// Return the minimum of two values.
-pub fn min<T>(left: T, right: T) -> T {
+pub fn min<T: PartialOrd>(left: T, right: T) -> T {
     if left <= right {
         left
     } else {
