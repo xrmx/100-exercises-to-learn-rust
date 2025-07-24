61b258cdf493d0f8efa65f56cc49b7794d2cb198
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2025-07-24 11:05:03 +0200

    04e13

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,29 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+
+struct DropBomb {
+    drop: bool
+}
+
+impl DropBomb {
+    fn new() -> Self {
+        Self{drop: true}
+    }
+
+    fn defuse(mut self) {
+        self.drop = false;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if self.drop == true {
+            panic!("Boooom")
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
