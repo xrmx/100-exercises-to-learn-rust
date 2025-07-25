cefc39fecaf0b2dad96b82a1184425ecc85bc6b2
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2025-07-25 12:29:09 +0200

    06e02

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,12 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut numbers = vec![0, 1];
+    let index = n as usize;
+    for i in 2..=index {
+        numbers.push(numbers[i-1] + numbers[i-2]);
+    }
+    numbers[index]
 }
 
 #[cfg(test)]
